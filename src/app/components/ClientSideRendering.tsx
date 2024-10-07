f079992de2f327475fe4c3174258506727d0850b
Turboyann <111585375+Turboyann@users.noreply.github.com>
2024-10-07 19:06:23 +0200

    Add files via upload

---
+++ src/app/components/ClientSideRendering.tsx
@@ -0,0 +1,105 @@
+'use client';
+import { useEffect, useState } from "react";
+import ListItem from "./ListItem";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+interface Post {
+  id: string;
+  title: string;
+  category: string;
+  date: string;
+  author: string;
+}
+
+type PostsData = Post[];
+
+interface ClientSideRenderingProps {
+  allPostsData: PostsData;
+}
+
+export default function ClientSideRendering({ allPostsData }: ClientSideRenderingProps) {
+  const [topic, setTopic] = useState<string>('');
+  const [posts, setPosts] = useState<PostsData>(allPostsData);
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  function handleSearch(term: string) {
+      const params = new URLSearchParams(searchParams);
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query')
+      }
+      replace(`${pathname}?${params.toString()}`)
+  }
+
+  function handleCategorySelection(topic: string) {
+    const params = new URLSearchParams(searchParams);
+    if (topic) {
+      params.set('category', topic);
+    } else {
+      params.delete('category')
+    }
+    replace(`${pathname}?${params.toString()}`)
+}
+
+  useEffect(() => {
+    const query = searchParams.get('query') || '';
+    const category = searchParams.get('category') || "";
+    setTopic(category);  
+    
+    let prefilteredPosts = topic !== ""
+      ? allPostsData.filter(post => post.category.toLowerCase() === topic.toLowerCase())
+      : allPostsData;
+
+    const filteredPosts = prefilteredPosts.filter(post =>
+    post.title.toLowerCase().includes(query.toLowerCase()));
+    
+    const sortedPosts = filteredPosts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()) 
+
+    setPosts(sortedPosts);
+  }, [topic, allPostsData]);
+
+  return (
+    <div>
+      <div className="flex flex-col sm:flex-row justify-center items-center p-6 mb-10 mt-10 sm:mt-0 shadow-md rounded-lg">
+          <div className="relative mr-4 mb-6 sm:mb-0">
+              <input
+                  type="text"
+                  placeholder="Suche Beiträge..."
+                  onChange={(e) => handleSearch(e.target.value)}
+                  defaultValue={searchParams.get('query')?.toString()}
+                  className="w-64 p-3 border border-gray-300 rounded-lg focus:outline-none focus:shadow-md"
+              />
+          </div>
+          <div className="flex justify-center sm:justify-start flex-wrap space-x-4">
+  {['Alles', 'Finanzen', 'Reisen', 'Bildung', 'Politik', 'Weiteres'].map((item) => (
+    <div
+      key={item}
+      onClick={() => {
+        const selectedTopic = item.toLowerCase() === 'alles' ? '' : item.toLowerCase();
+        setTopic(selectedTopic);
+        handleCategorySelection(selectedTopic);
+      }}
+      className={`cursor-pointer px-4 py-2 mb-2 lg:mb-0 bg-white border border-gray-300 rounded-lg text-gray-700 hover:bg-blue-100 transition duration-200 ${topic === (item.toLowerCase() === 'alles' ? '' : item.toLowerCase()) ? 'bg-blue-100' : ''}`}
+    >
+      {item}
+    </div>
+  ))}
+</div>
+    </div>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 mx-auto w-3/4 gap-5">
+        
+      {posts.length > 0 ? (
+          posts.map(post => (
+            <ListItem key={post.id} post={post} />
+          ))
+        ) : (
+          <li className="col-span-full text-center text-gray-500 mt-20 md:text-2xl h-[300px] lg:h-[500px]">Keine Suchergebnisse gefunden.</li>
+        )}
+
+      </ul>
+    </div>
+  );
+}
