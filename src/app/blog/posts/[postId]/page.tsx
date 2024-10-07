6c26872b161ca99434beb276763c7ebfaa9a5a78
Turboyann <111585375+Turboyann@users.noreply.github.com>
2024-10-07 23:08:07 +0200

    Add files via upload

---
+++ src/app/blog/posts/[postId]/page.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { getSortedPostsData, getPostData } from '../../../../../lib/posts'
+import Link from 'next/link'
+import { notFound } from "next/navigation"
+import "../../../globals.css"
+import getFormattedDate from '../../../../../lib/getFormattedDate'
+import Image from 'next/image'
+import Commentsection from "../../../components/Commentsection"
+
+export function generateMetadata({ params }: { params: {postId: string }}) {
+  
+    const posts = getSortedPostsData()
+    const { postId } = params
+
+    const post = posts.find((post) => post.id === postId);
+
+    if (!post) {
+        return {
+            title: 'Post not Found'
+        }
+    }
+
+    return {
+        title: post.title,
+    }
+}
+
+export default async function Post({ params }: { params: {postId: string }}) {
+  
+    const posts = getSortedPostsData()
+    const { postId } = params
+
+    const post = posts.find((post) => post.id === postId);
+    if (!post) {
+        return notFound();
+    }
+
+    const { title, date, category, author, contentHtml } = await getPostData(postId)
+
+    const pubDate = getFormattedDate(date)
+  
+    return (
+    <div className='mx-auto w-6/12 my-20'>
+        <h1 className='text-7xl mt-8 mb-6 w-9/12 mx-auto'>{title}</h1>
+        <div className='mb-6 w-9/12 mx-auto'>
+            <p className='inline mt-0 mr-6'>{pubDate}</p>
+            <Link href={`/blog?category=${category}`} className='inline mt-0 mx-6'>{category}</Link>
+            <p className='inline mt-0 mx-6'>{author}</p>
+        </div>        
+        <Image className="mb-12 w-full" src='/steuer.jpg' width={5568} height={3712} alt="image"/>
+        <article className='w-9/12 mx-auto mb-20'>
+            <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        </article>
+        <Commentsection postId={postId}/>
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+import ListItemNew from "./components/ListItemNew";
+import { getSortedPostsData } from "../../lib/posts";
+import "./globals.css";
+
+const allPostsData = getSortedPostsData();
+
+export default function Home() {
+
+  return (
+    <main className="">
+
+    <ListItemNew allPostsData={allPostsData}/>
+
+    <div className="background-secondary h-[400] mb-32">
+      <h1 className="text-3xl md:text-4xl text-color-primary mt-14 md:mt-0 pb-20 font-light">Erkunde unsere Themen</h1>
+      <ul className="grid grid-cols-2  sm:grid-cols-3 md:grid-cols-5 gap-6">
+        <Link href="/blog?category=finanzen" className="topic-image-element">
+          <Image className="topic-images" src='/finanzen.jpg' alt="finanzen" width={3556} height={3556} />
+          <p className="home-topics-title">Finanzen</p>
+        </Link>
+        <Link href="/blog?category=reisen" className="topic-image-element">
+          <Image className="topic-images" src='/reisen.jpg' alt="reisen" width={2000} height={2000} />
+          <p className="home-topics-title">Reisen</p>
+        </Link>
+        <Link href="/blog?category=bildung" className="topic-image-element">
+          <Image className="topic-images" src='/bildung.jpg' alt="bildung" width={3163} height={3163} />
+          <p className="home-topics-title">Bildung</p>
+        </Link>
+        <Link href="/blog?category=politik" className="topic-image-element">
+          <Image className="topic-images" src='/politik.jpg' alt="politik" width={2907} height={2907} />
+          <p className="home-topics-title">Politik</p>
+        </Link>
+        <Link href="/blog?category=mehr" className="topic-image-element">
+          <Image className="topic-images" src='/mehr.jpg' alt="mehr" width={4000} height={4000} />
+          <p className="home-topics-title">Weiteres</p>
+        </Link>
+      </ul>
+    </div>
+
+    <div className="secondary-bg p-5 md:p-10">
+      <ul className="flex flex-col md:flex-row justify-between w-1/2 md:w-2/3 my-16 xl:my-0 space-x-16 mx-auto relative md:w-2/3 lg:w-1/2">
+        <li className="flex flex-col content-center justify-center w-full md:mb-0 mx-auto">
+          <h1 className="text-color-primary text-xl md:text-2xl lg:text-3xl mb-2">Was über die Seite</h1>
+          <p className="text-base md:text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis accusantium ut, dolor architecto consectetur incidunt sit sed hic qui odit minima, molestias ducimus dicta, sunt adipisci rem dolore voluptatum labore veniam amet in dolorum est. Praesentium fuga repudiandae ducimus cumque omnis! Consequatur hic iure veritatis commodi ut doloribus quaerat dolore!
+          </p>
+        </li>
+        <li className="hidden xl:flex justify-center w-full md:w-1/2">
+          <Image className="about-the-page wavy-image" src='/about-the-page.jpg' alt="about-the-page" width={7360} height={4912} />
+        </li>
+      </ul>
+    </div>
+    </main>
+  );
+}
